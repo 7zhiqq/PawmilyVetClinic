8249063d9ca96b749c482536bc957c5bf8fbf294
Kheishia Faith Mationg <kfgmationg@gmail.com>
2026-03-31 00:13:47 +0800

    logout button in responsive

--- website/static/js/navbar.js
+++ website/static/js/navbar.js
@@ -3,8 +3,22 @@
   var hamburger  = document.getElementById('hamburger');
   var mobileMenu = document.getElementById('mobileMenu');
   if (hamburger && mobileMenu) {
+    var setExpanded = function (expanded) {
+      hamburger.setAttribute('aria-expanded', expanded ? 'true' : 'false');
+    };
+
     hamburger.addEventListener('click', function () {
-      mobileMenu.classList.toggle('open');
+      var isOpen = mobileMenu.classList.toggle('open');
+      setExpanded(isOpen);
     });
+
+    mobileMenu.querySelectorAll('a').forEach(function (link) {
+      link.addEventListener('click', function () {
+        mobileMenu.classList.remove('open');
+        setExpanded(false);
+      });
+    });
+
+    setExpanded(false);
   }
 })();
